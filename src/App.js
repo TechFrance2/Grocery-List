6ceff6b817bdefc079020832754b420d28c5e435
Franci Nakamura <francinakamura@FranciNrasiMac3.attlocal.net>
2020-05-14 19:37:22 -0700

    added handle change & changed style when checkbox is marked off

--- src/App.js
+++ src/App.js
@@ -5,16 +5,43 @@ import './App.css'
 
 
 
-function App() {
-  const groceryShop = groceryItems.map(item => <Groceries key= {item.id} item={item} />)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        groceries: groceryItems,    
+    };
+    this.handleChange = this.handleChange.bind(this)
+  }
+    handleChange(id) {
+      this.setState((prevState) => {
+        const groceryUpdate = prevState.groceries.map((grocery) => {
+          if (grocery.id === id) {
+            grocery.completed = !grocery.completed;
+          }
+          return grocery;
+        });
 
-  return (
-    <div className= "shop-list">
-       <h1> Grocery List </h1>
-       {groceryShop}   
-       
-    </div>
-  );
+        return {
+          groceries: groceryUpdate, 
+        };
+      });
+    }
+  
+  render() {
+    const grocery = this.state.groceries.map((item ) => (
+    <Groceries key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return (
+      <div className= "shop-list">
+         <h1> Grocery List </h1>
+         {grocery}   
+         
+      </div>
+    )
+  } 
 }
 
 export default App;
+
